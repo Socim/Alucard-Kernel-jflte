c7417d3a1680eeb5ff701f04b27c07f5768a0895
Socim <Elabiv@gmail.com>
2014-04-21 08:57:20 +0800

    Fix path

--- clean-all-4.10.sh
+++ clean-all-4.10.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp -pv .config .config.bkp;
-make ARCH=arm CROSS_COMPILE=../../linaro-arm-eabi-4.10/bin/arm-eabi- mrproper;
+make ARCH=arm CROSS_COMPILE=../linaro-arm-eabi-4.10/bin/arm-eabi- mrproper;
 cp -pv .config.bkp .config;
 make clean && make mrproper;
 rm -rf ./kernel/usr;
